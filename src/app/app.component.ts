9f6806376e4024b3e379a356727eec6712a38468
Phodal Huang <h@phodal.com>
2020-03-24 21:43:58 +0800

    build: init build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Injector} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wc-devops-path',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'devops-path';
+  constructor(injector: Injector) {
+    const CEElement = createCustomElement(PeriodicTableComponent, {injector});
+    customElements.define('devops-path', CEElement);
+  }
 }
